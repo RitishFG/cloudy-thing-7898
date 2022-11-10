a33dbe98367a37a1c84a58fe0f2287b8215146e6
sujjwal21 <sujjwal85@gmail.com>
2022-11-10 21:41:58 +0530

    day-2

--- Scripts/online_course.js
+++ Scripts/online_course.js
@@ -72,6 +72,26 @@ new_price:"₹ 2999",
 title:"R Programming Course"
 }
 ]
+import {navbar} from "../Components/nav.js";
+let nav=document.getElementById("navbar")
+nav.innerHTML=navbar();
+
+
+
+
+const openMenu = document.querySelector("#show-menu");
+
+const closeMenu = document.querySelector("#hide-menu");
+
+const sideMenu = document.querySelector("#nav-menu");
+
+openMenu.addEventListener("click", function(){
+    sideMenu.classList.add('active')
+})
+
+closeMenu.addEventListener("click", function(){
+    sideMenu.classList.remove('active')
+})
 
 appendData(course)
 
